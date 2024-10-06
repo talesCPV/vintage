73bde7df8a6471d085f2efd9166b45285aafb797
talesCPV <tales@flexibus.com.br>
2024-10-06 15:42:06 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -24,8 +24,8 @@ then
     
     git commit -m "by_script"
 
-    #git branch -M main
-    #git remote add origin git@github.com:talesCPV/vintage.git
+    git branch -M main
+    git remote add origin git@github.com:talesCPV/vintage.git
     git remote set-url origin git@github.com:talesCPV/vintage.git
 
     git push -u -f origin main
