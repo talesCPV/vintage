0af55c2f4d0dfc0a03f7bcd6dc83e21e5a651cf4
talesCPV <tales@flexibus.com.br>
2024-10-17 16:48:59 -0300

    by_script

--- backend/delFile.php
+++ backend/delFile.php
@@ -1,6 +1,6 @@
 <?php   
 	if (IsSet($_POST["path"])){
-        $path = getcwd().$_POST["path"];
+        $path = getcwd().$_POST["path"];        
         if (file_exists($path)) {
             unlink($path);
         }
