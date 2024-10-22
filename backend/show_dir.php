dc3a49be498849286612d97908b06b101a1ec8c7
talesCPV <tales@flexibus.com.br>
2024-10-22 16:57:39 -0300

    by_script

--- backend/show_dir.php
+++ backend/show_dir.php
@@ -2,6 +2,7 @@
 
     if (IsSet($_POST["dir"])){
         $path = getcwd().'/../'.$_POST["dir"];
+//echo $path;        
         $files = scandir($path);
         $resp = json_encode($files);
         print($resp); 
